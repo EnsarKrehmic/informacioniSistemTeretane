55e5e969a746fb4b102ab320bc2bd9fea9529514
EnsarKrehmic <krehmiicjr@gmail.com>
2025-06-05 12:43:04 +0200

    Implementacija: Dockerizacija
    Izvršena je dockerizacija cijele web-aplikacije kao i baze podataka.

--- InformacioniSistemTeretane/InformacioniSistemTeretane/Program.cs
+++ InformacioniSistemTeretane/InformacioniSistemTeretane/Program.cs
@@ -7,7 +7,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? Environment.GetEnvironmentVariable("ASPNETCORE_ConnectionStrings__DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
